5fea1b34ee791457e715cbba3aba8735ee425783
NHAT VU HOANG MINH <NHAT VU HOANG MINH>
2017-05-11 09:01:30 +0700

    add config example todo task

---
+++ client/js/app.js
@@ -0,0 +1,32 @@
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-example-angular
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+angular
+  .module('app', [
+    'lbServices',
+    'ui.router',
+	'com.module.core',
+	'com.module.about',
+	'com.module.events',
+	'com.module.files',
+	'com.module.notes',
+	'com.module.pages',
+	'com.module.posts',
+	'com.module.products',
+	'com.module.sandbox',
+	'com.module.settings',
+	'com.module.users'
+  ])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
+      $urlRouterProvider) {
+    $stateProvider
+      .state('todo', {
+        url: '',
+        templateUrl: 'views/todo.html',
+        controller: 'TodoController'
+      });
+
+    $urlRouterProvider.otherwise('todo');
+  }]);
